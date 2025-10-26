1221e026295189c53d9c5b43e61243538778aa12
Adrian Preuß <1371300+Bizarrus@users.noreply.github.com>
2025-10-26 14:39:58 +0100

    Adding docs

--- src/enums/CreatorType.ts
+++ src/enums/CreatorType.ts
@@ -3,11 +3,33 @@
  * @since       1.0.0
  */
 
+/**
+* Represents the type of an creator.
+*/
 const enum CreatorType {
+    /**
+     * Unknown creator type.
+     */
     UNKNOWN,
+
+    /**
+     * Represents a player creator (for example, a normal user)
+     */
     PLAYER,
+
+    /**
+     * Represents an internal creator (for sample DICE, EA,..)
+     */
     INTERNAL,
+
+    /**
+     * Represents an external creator (for example, a third-party company working for DICE, EA...)
+     */
     EXTERNAL,
+
+    /**
+     * Represents a trusted creator (for example, a verified content creator)
+     */
     TRUSTED
 }
 
